3cd8203bd3cc267d5220ee6eeacc40c537174cd5
Vlad897333 <147833489+Vlad897333@users.noreply.github.com>
2025-02-27 11:28:56 +0300

    manual_test.py

--- manual_test.py
+++ manual_test.py
@@ -1,127 +1,127 @@
 import Test_2
 
 def test_stroka():
-    if Test_2.back_string("Hello boy") == "yob olleH":
-        print("Test stroka(Hello boy) is OK ")
+    if Test_2.back_string("Hello") == "olleH":
+        print("Test stroka(Hello) is OK ")
     else:
-        print("Test stroka(Hello boy) is Fail")
+        print("Test stroka(Hello) is Fail")
 
 def test_stroka():
-    if Test_2.back_string("Car") == "raC":
-        print("Test stroka(Car) is OK ")
+    if Test_2.back_string("Trem") == "merT":
+        print("Test stroka(Trem) is OK ")
     else:
-        print("Test stroka(Car) is Fail")
+        print("Test stroka(Trem) is Fail")
 
 def test_stroka():
-    if Test_2.back_string("Tools") == "slooT":
-        print("Test stroka(Tools) is OK ")
+    if Test_2.back_string("Praaf") == "faarP":
+        print("Test stroka(Praaf) is OK ")
     else:
-        print("Test stroka(Tools) is Fail")
+        print("Test stroka(Praaf) is Fail")
 
 def test_stroka():
-    if Test_2.back_string("World") == "dlroW":
-        print("Test stroka(World) is OK ")
+    if Test_2.back_string("jili") == "ilij":
+        print("Test stroka(jili) is OK ")
     else:
-        print("Test stroka(World) is Fail")
+        print("Test stroka(jili) is Fail")
 
 def test_stroka():
-    if Test_2.back_string("Wolf") == "floW":
-        print("Test stroka(Wolf) is OK ")
+    if Test_2.back_string("kat") == "tak":
+        print("Test stroka(kat) is OK ")
     else:
-        print("Test stroka(Wolf) is Fail")
+        print("Test stroka(kat) is Fail")
 
 
-#------------------------------------------------------------------------------
-def test_polindrome():
-    if Test_2.polindrome("Anastasia") == "Anastasia":
-        print("Test polindrome(Anastasia) is OK")
-    else:
-        print("Test polindrome(Anastasia) is Fail")
 
 def test_polindrome():
-    if Test_2.polindrome("stats") == "stats":
-        print("Test polindrome(stats) is OK")
+    if Test_2.polindrome("Masha") == "ahsaM":
+        print("Test polindrome(Masha) is OK")
     else:
-        print("Test polindrome(stats) is Fail")
+        print("Test polindrome(Masha) is Fail")
 
 def test_polindrome():
-    if Test_2.polindrome("refer") == "refer":
-        print("Test polindrome(refer) is OK")
+    if Test_2.polindrome("Reall") == "llaeR":
+        print("Test polindrome(Reall) is OK")
     else:
-        print("Test polindrome(refer) is Fail")
+        print("Test polindrome(Reall) is Fail")
 
 def test_polindrome():
-    if Test_2.polindrome("madam") == "madam":
-        print("Test polindrome(madam) is OK")
+    if Test_2.polindrome("feer") == "reef":
+        print("Test polindrome(feer) is OK")
     else:
-        print("Test polindrome(madam) is Fail")
+        print("Test polindrome(feer) is Fail")
 
 def test_polindrome():
-    if Test_2.polindrome("levels") == "levels":
-        print("Test polindrome(levels) is OK")
+    if Test_2.polindrome("man") == "nam":
+        print("Test polindrome(man) is OK")
     else:
-        print("Test polindrome(levels) is Fail")
-#------------------------------------------------------------------------------
-def test_voweles():
-    if Test_2.vowels("Шишка") == 1 :
-        print("Test vowels(Шишка) is OK")
+        print("Test polindrome(man) is Fail")
+
+def test_polindrome():
+    if Test_2.polindrome("life") == "efil":
+        print("Test polindrome(life) is OK")
     else:
-        print("Test vowels(Шишка) is Fail")
+        print("Test polindrome(life) is Fail")
 
 def test_voweles():
-    if Test_2.vowels("Машинка") == 3 :
-        print("Test vowels(Машинка) is OK")
+    if Test_2.vowels("кокос") == 1 :
+        print("Test vowels(кокос) is OK")
     else:
-        print("Test vowels(Машинка) is Fail")
+        print("Test vowels(кокос) is Fail")
 
 def test_voweles():
-    if Test_2.vowels("Собачка") == 3 :
-        print("Test vowels(Собачка) is OK")
+    if Test_2.vowels("рукав") == 3 :
+        print("Test vowels(рукав) is OK")
     else:
-        print("Test vowels(Собачка) is Fail")
+        print("Test vowels(рукав) is Fail")
 
 def test_voweles():
-    if Test_2.vowels("Моль") == 2 :
-        print("Test vowels(Моль) is OK")
+    if Test_2.vowels("терьер") == 3 :
+        print("Test vowels(терьер) is OK")
     else:
-        print("Test vowels(Моль) is Fail")
+        print("Test vowels(терьер) is Fail")
 
 def test_voweles():
-    if Test_2.vowels("Универ") == 2 :
-        print("Test vowels(Универ) is OK")
+    if Test_2.vowels("кролик") == 2 :
+        print("Test vowels(кролик) is OK")
     else:
-        print("Test vowels(Универ) is Fail")
-#------------------------------------------------------------------------------
-def test_duplicate():
-    if Test_2.remove_duplicates("Hello world") == "Helo wrd":
-        print("Test duplicate(Hello world) is OK")
+        print("Test vowels(кролик) is Fail")
+
+def test_voweles():
+    if Test_2.vowels("Шарага") == 2 :
+        print("Test vowels(Шарага) is OK")
     else:
-        print("Test duplicate(Hello world) is Fail")
+        print("Test vowels(Шарага) is Fail")
 
 def test_duplicate():
-    if Test_2.remove_duplicates("he knows Inglish") == "he knows gli":
-        print("Test duplicate(he knows Inglish) is OK")
+    if Test_2.remove_duplicates("cold") == "dloc":
+        print("Test duplicate(cold) is OK")
     else:
-        print("Test duplicate(he knows Inglish) is Fail")
+        print("Test duplicate(cold) is Fail")
 
 def test_duplicate():
-    if Test_2.remove_duplicates("qweqweqwe") == "qwe":
-        print("Test duplicate(qweqweqwe) is OK")
+    if Test_2.remove_duplicates("lish") == "he knows Lish":
+        print("Test duplicate(lish) is OK")
     else:
-        print("Test duplicate(qweqweqwe) is Fail")
+        print("Test duplicate(lish) is Fail")
 
 def test_duplicate():
-    if Test_2.remove_duplicates("sjkdghkjsd") == "asd":
-        print("Test duplicate(sjkdghkjsd) is OK")
+    if Test_2.remove_duplicates("rtyrtyrty") == "rty":
+        print("Test duplicate(rtyrtyrty) is OK")
     else:
-        print("Test duplicate(sjkdghkjsd) is Fail")
+        print("Test duplicate(rtyrtyrty) is Fail")
 
 def test_duplicate():
-    if Test_2.remove_duplicates("lklklklklkA") == "zxc":
-        print("Test duplicate(lklklklklkA) is OK")
+    if Test_2.remove_duplicates("shjhjhg") == "asd":
+        print("Test duplicate(shjhjhg) is OK")
     else:
-        print("Test duplicate(lklklklklkA) is Fail")
-#-----------------------------------------------------------------------------
+        print("Test duplicate(shjhjhg) is Fail")
+
+def test_duplicate():
+    if Test_2.remove_duplicates("kikikiki") == "ui":
+        print("Test duplicate(kikikiki) is OK")
+    else:
+        print("Test duplicate(kikikiki) is Fail")
+
 test_stroka()
 test_polindrome()
 test_voweles()
